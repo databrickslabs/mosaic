53ebedbe985cc5048554e8f827de5cab469218f5
sllynn <stuart.lynn@databricks.com>
2021-12-21 21:59:59 +0000

    added basic type checks

--- src/test/scala/com/databricks/mosaic/expressions/geometry/TypeCheckSuite.scala
+++ src/test/scala/com/databricks/mosaic/expressions/geometry/TypeCheckSuite.scala
@@ -3,7 +3,6 @@ package com.databricks.mosaic.expressions.geometry
 import org.scalatest.{FunSuite, Matchers}
 import com.databricks.mosaic.test.SparkTest
 import com.databricks.mosaic.mocks.{getWKTRowsDf, getHexRowsDf}
-import org.apache.spark.sql.Row
 import org.apache.spark.sql.functions.col
 import com.databricks.mosaic.functions.{as_hex, st_geometrytype}
 
