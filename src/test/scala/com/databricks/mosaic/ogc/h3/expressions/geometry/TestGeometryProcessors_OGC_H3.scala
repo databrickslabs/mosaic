010c40f3271bbbee87825704a498bef2fc5e9650
sllynn <stuart.lynn@databricks.com>
2022-01-12 10:48:00 +0000

    added test for ST_Contains with ESRI backend

--- src/test/scala/com/databricks/mosaic/ogc/h3/expressions/geometry/TestGeometryProcessors_OGC_H3.scala
+++ src/test/scala/com/databricks/mosaic/ogc/h3/expressions/geometry/TestGeometryProcessors_OGC_H3.scala
@@ -15,6 +15,28 @@ class TestGeometryProcessors_OGC_H3 extends FunSuite with Matchers with SparkTes
   import mosaicContext.functions._
   import testImplicits._
 
+  test("Test polygon contains point") {
+    mosaicContext.register(spark)
+    
+    val poly = 
+      """POLYGON ((10 10, 110 10, 110 110, 10 110, 10 10),
+        | (20 20, 20 30, 30 30, 30 20, 20 20),
+        | (40 20, 40 30, 50 30, 50 20, 40 20))""".stripMargin.filter(_ >= ' ')
+
+    val rows = List(
+      (poly, "POINT (35 25)", true), 
+      (poly, "POINT (25 25)", false)
+      )
+
+    val results = 
+      rows.toDF("leftGeom", "rightGeom", "expected")
+      .withColumn("result", st_contains($"leftGeom", $"rightGeom"))
+      .where($"expected" === $"result")
+
+    results.count shouldBe 2
+
+  }
+
     test("Test convex hull generation") {
     mosaicContext.register(spark)
     val multiPoint = List("MULTIPOINT (-70 35, -80 45, -70 45, -80 35)")
