26f098a6d2e159afb23284b35e827a06a679d0fd
erni.durdevic@gmail.com <erni.durdevic@gmail.com>
2022-03-04 15:49:21 +0000

    Improved pipeline example

--- notebooks/examples/ExamplePipeline.py
+++ notebooks/examples/ExamplePipeline.py
@@ -8,19 +8,52 @@
 
 # COMMAND ----------
 
+import pyspark.sql.functions as f
+
+# COMMAND ----------
+
 environment = 'erni' # This could be: prod / dev / test / etc.
 
 # NOTE: It is recommended to use an external bucket instead of DBFS in your production pipelines
 
+# Raw data
 raw_path = f'dbfs:/tmp/mosaic_exmaple/{environment}/raw'
+
+# Bronze
 bronze_path = f'dbfs:/tmp/mosaic_exmaple/{environment}/bronze'
+bronze_db = f'{environment}_bronze'
+
+# Silver
 silver_path = f'dbfs:/tmp/mosaic_exmaple/{environment}/silver'
+silver_db = f'{environment}_silver'
+
+# Gold
 # It is recommended to store the gold in a separate bucket from the rest, in order to
 # prevent production jobs on bronze/silver to slow down end-user queries on gold tables
-gold_path = f'dbfs:/tmp/mosaic_exmaple/{environment}/gold' 
+gold_path = f'dbfs:/tmp/mosaic_exmaple/{environment}/gold'
+gold_db = f'{environment}_gold'
+
+# Metadata
+metadata_path = f'dbfs:/tmp/mosaic_exmaple/{environment}/metadata'
 
 # COMMAND ----------
 
+dbutils.fs.rm(raw_path, True)
+dbutils.fs.rm(bronze_path, True)
+dbutils.fs.rm(silver_path, True)
+dbutils.fs.rm(gold_path, True)
+dbutils.fs.rm(metadata_path, True)
+
+# COMMAND ----------
+
+# Create databases
+spark.sql(f"CREATE DATABASE IF NOT EXISTS {bronze_db} LOCATION '{bronze_path}'")
+spark.sql(f"CREATE DATABASE IF NOT EXISTS {silver_db} LOCATION '{silver_path}'")
+spark.sql(f"CREATE DATABASE IF NOT EXISTS {gold_db} LOCATION '{gold_path}'")
+
+# COMMAND ----------
+
+# MAGIC 
 # MAGIC %md 
 # MAGIC ## Land raw data
 # MAGIC 
@@ -28,18 +61,52 @@ gold_path = f'dbfs:/tmp/mosaic_exmaple/{environment}/gold'
 
 # COMMAND ----------
 
+
+def simulate_random_nyc_taxi_data(n_rows):
+  
+  ts_start_ms = 1516364153
+  ts_span_s = 365 * 24 * 60 * 60
+  trip_max_duration_s = 30 * 60
+  
+  min_lat = 40.513189
+  max_lat = 40.884033
+  
+  min_long = -74.279095
+  max_long = -73.672070
+  
+  return (spark.range(n_rows)
+          .withColumn("rand_timestamp", (f.lit(ts_start_ms) + f.rand() * ts_span_s).cast("long"))
+          .withColumn("tpep_pickup_datetime", f.col("rand_timestamp").cast("timestamp"))
+          .withColumn("tpep_dropoff_datetime", (f.col("rand_timestamp") + f.rand() * trip_max_duration_s).cast("timestamp"))
+          
+          .withColumn("Dropoff_longitude", f.rand() * (max_long - min_long) + min_long)
+          .withColumn("Dropoff_latitude", f.rand() * (max_lat - min_lat) + min_lat)
+          .withColumn("Pickup_longitude", f.rand() * (max_long - min_long) + min_long)
+          .withColumn("Pickup_latitude", f.rand() * (max_lat - min_lat) + min_lat)
+          
+          .withColumn("Passenger_count", (f.rand() * 4 + 1).cast("int"))
+          
+          .withColumn("Fare_amount", (f.rand() * 50 * 100 + 500).cast("int") / 100)
+          .withColumn("Tip_amount", (f.rand() * 15 * 100).cast("int") / 100)
+          .withColumn("Total_amount", f.col("Tip_amount") + f.col("Tip_amount"))
+          
+          .drop("rand_timestamp")
+         )
+
+# COMMAND ----------
+
 import requests
 import os
 import pathlib
 
-yellow_taxi_trips_urls = [ 
-  'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-01.csv',
-  'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-02.csv',
-#   'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-03.csv',
-#   'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-04.csv',
-#   'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-05.csv',
-#   'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-06.csv',
-]
+# yellow_taxi_trips_urls = [ 
+#   'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-01.csv',
+#   'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-02.csv',
+# #   'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-03.csv',
+# #   'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-04.csv',
+# #   'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-05.csv',
+# #   'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-06.csv',
+# ]
 
 taxi_zones_urls = [ 
   'https://data.cityofnewyork.us/api/geospatial/d3c5-ddgc?method=export&format=GeoJSON' 
@@ -48,7 +115,6 @@ taxi_zones_urls = [
 # The DBFS file system is mounted under /dbfs/ directory on Databricks cluster nodes
 raw_local_path = pathlib.Path(raw_path.replace('dbfs:/', '/dbfs/')) 
 
-raw_yellow_taxi_trip_path = raw_local_path / 'yellow_taxi_trip'
 raw_taxi_zones_path = raw_local_path / 'taxi_zones'
 
 def download(urls, folder, extension='csv'):
@@ -68,11 +134,12 @@ def download(urls, folder, extension='csv'):
 
 # COMMAND ----------
 
-download(yellow_taxi_trips_urls, raw_yellow_taxi_trip_path, 'csv')
-
-# COMMAND ----------
-
-dbutils.fs.ls(raw_nyc_yellow_taxi_trip.as_uri())
+(simulate_random_nyc_taxi_data(1000000)
+   .write
+   .option("header", True)
+   .mode("append")
+   .csv(f"{raw_path}/yellow_taxi_trip")
+)
 
 # COMMAND ----------
 
@@ -97,10 +164,20 @@ download(taxi_zones_urls, raw_taxi_zones_path, 'geojson')
 
 # COMMAND ----------
 
-def raw_to_bronze_taxi_trip(input_path, output_path):
+
+def write_stream(df, output_table, checkpoint_path):
+  # Start the stream with trigger once.
+  return (df.writeStream.format("delta")
+      .option("checkpointLocation", checkpoint_path)
+      .option("mergeSchema", "true")
+      .trigger(once=True)
+      .toTable(output_table)
+    )
+
+def raw_to_bronze_yellow_taxi_trip(input_path, metadata_path, output_table):
   
-  checkpoint_path = f"{output_path}/_checkpoints"
-  schema_path = f"{output_path}/_schema"
+  checkpoint_path = f"{metadata_path}/_checkpoints/{output_table}"
+  schema_path = f"{metadata_path}/_schema/{output_table}"
 
   # Read data as stream with auto loader
   df = (spark
@@ -114,47 +191,39 @@ def raw_to_bronze_taxi_trip(input_path, output_path):
         .load(input_path)
        )
 
-  # Start the stream with trigger once.
-  (df.writeStream.format("delta")
-    .option("checkpointLocation", checkpoint_path)
-    .option("mergeSchema", "true")
-    .trigger(once=True)
-    .start(output_path)
-  )
+  return write_stream(df, output_table, checkpoint_path)
   
-def raw_to_bronze_zones(input_path, output_path):
   
-  checkpoint_path = f"{output_path}/_checkpoints"
-  schema_path = f"{output_path}/_schema"
+def raw_to_bronze_taxi_zones(input_path, metadata_path, output_table):
+  
+  checkpoint_path = f"{metadata_path}/_checkpoints/{output_table}"
+  schema_path = f"{metadata_path}/_schema/{output_table}"
 
   # Read data as stream with auto loader
   df = (spark
         .readStream
         .format("cloudFiles")
         .option("cloudFiles.format", "json")
-        # By specifying an empty schema location, auto loader will 
-        # automatically infer the schema from the input files
+        .option("cloudFiles.inferColumnTypes", True)
         .option("cloudFiles.schemaLocation", schema_path)
         .option("multiline", "true") # Geojson has a multiline JSON format
         .load(input_path)
        )
 
-  # Start the stream with trigger once.
-  (df.writeStream.format("delta")
-    .option("checkpointLocation", checkpoint_path)
-    .option("mergeSchema", "true")
-    .trigger(once=True)
-    .start(output_path)
-  )
+  return write_stream(df, output_table, checkpoint_path)
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ### Tellow taxi trip
+# MAGIC ### Yellow taxi trip
 
 # COMMAND ----------
 
-raw_to_bronze_taxi_trip(f"{raw_path}/yellow_taxi_trip", f"{bronze_path}/yellow_taxi_trip")
+raw_to_bronze_yellow_taxi_trip(
+  f"{raw_path}/yellow_taxi_trip/*.csv", 
+  metadata_path, 
+  f"{bronze_db}.yellow_taxi_trip"
+).awaitTermination()
 
 # COMMAND ----------
 
@@ -163,15 +232,96 @@ raw_to_bronze_taxi_trip(f"{raw_path}/yellow_taxi_trip", f"{bronze_path}/yellow_t
 
 # COMMAND ----------
 
-raw_to_bronze_zones(f"{raw_path}/taxi_zones", f"{bronze_path}/taxi_zones")
+raw_to_bronze_taxi_zones(
+  f"{raw_path}/taxi_zones",
+  metadata_path,
+  f"{bronze_db}.taxi_zones"
+).awaitTermination()
 
 # COMMAND ----------
 
-import pyspark.sql.functions as f
+# MAGIC %md
+# MAGIC ## Silver
+# MAGIC 
+# MAGIC Silver tables contain cleaned and formatted data ready to be used.
 
-display(df.select(
- "features",
- "type",
- f.explode(f.col("features.properties")).alias("properties")
-).select("*",f.explode(f.col("features.geometry")).alias("geometry")).drop("features")
-       )
+# COMMAND ----------
+
+spark.table(f"{bronze_db}.yellow_taxi_trip").display()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Functions
+
+# COMMAND ----------
+
+def clean_yellow_taxi_trip(df):
+  return (df
+
+   # Standardize column naming and select columns of interest
+   .select(
+     f.col("tpep_pickup_datetime").alias("pickup_time"),
+     f.col("tpep_dropoff_datetime").alias("dropoff_time"),
+     f.col("Pickup_longitude").alias("pickup_longitude"),
+     f.col("Pickup_latitude").alias("pickup_latitude"),
+     f.col("Dropoff_longitude").alias("dropoff_longitude"),
+     f.col("Dropoff_latitude").alias("dropoff_latitude"),
+
+     f.col("Fare_amount").alias("fare_amount_usd"),
+     f.col("Tip_amount").alias("tip_amount_usd"),
+     f.col("Total_amount").alias("total_amount_usd")
+   )
+ 
+   # Standardize data types
+   .withColumn("pickup_time", f.col("pickup_time").cast("timestamp"))
+   .withColumn("dropoff_time", f.col("dropoff_time").cast("timestamp"))
+                       
+   # Add data quality metadata
+   # Here you cound use more sofisticated checks with a library like https://greatexpectations.io/ or https://github.com/awslabs/deequ
+   .withColumn("is_valid", f.col("total_amount_usd").between(0, 1000))
+)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Tests
+
+# COMMAND ----------
+
+# TODO
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Yellow taxi trip
+
+# COMMAND ----------
+
+(spark
+   .table(f"{bronze_db}.yellow_taxi_trip")
+   .transform(clean_yellow_taxi_trip)
+   .write
+   .mode("overwrite")
+   .saveAsTable(f"{silver_db}.yellow_taxi_trip_clean")
+)
+
+# COMMAND ----------
+
+df = spark.table(f"{bronze_db}.taxi_zones")
+
+
+# COMMAND ----------
+
+def clean_taxi_zones(df):
+  return (df
+          .select("type", f.explode(f.col("features")).alias("feature"))
+          .select("type", f.col("feature.properties").alias("properties"), f.col("feature.geometry").alias("geometry"))
+         )
+
+display(clean_taxi_zones(spark.table(f"{bronze_db}.taxi_zones")))
+
+
+# COMMAND ----------
+
+
