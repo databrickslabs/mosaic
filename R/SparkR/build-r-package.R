2a3ff67d1682400959fe3f76e9ae882d2c7b385c
sllynn <stuart.lynn@databricks.com>
2022-04-26 23:34:18 +0100

    add back scala tests and python build

--- R/SparkR/build-r-package.R
+++ R/SparkR/build-r-package.R
@@ -1,7 +1,7 @@
 repo<-"https://cran.rstudio.com/"
 
-# install.packages("devtools", repos=repo)
-# install.packages("roxygen2", repos=repo)
+install.packages("devtools", repos=repo)
+install.packages("roxygen2", repos=repo)
 devtools::install_github("apache/spark@v3.2.1", subdir='R/pkg')
 
 library(devtools)
