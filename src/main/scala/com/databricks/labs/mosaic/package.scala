06d09cf2addcbc8cd879a89c20035fc8bf87ec9f
milos.colic <milos.colic@databricks.com>
2022-05-28 20:06:17 +0100

    Add missing reference for BNG to package object.

--- src/main/scala/com/databricks/labs/mosaic/package.scala
+++ src/main/scala/com/databricks/labs/mosaic/package.scala
@@ -8,5 +8,6 @@ package object mosaic {
     val JTS: GeometryAPI = mosaic.core.geometry.api.GeometryAPI.JTS
     val ESRI: GeometryAPI = mosaic.core.geometry.api.GeometryAPI.ESRI
     val H3: IndexSystem = mosaic.core.index.H3IndexSystem
+    val BNG: IndexSystem = mosaic.core.index.BNGIndexSystem
 
 }
