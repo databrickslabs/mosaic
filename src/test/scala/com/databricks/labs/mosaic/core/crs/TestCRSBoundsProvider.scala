1aa6395252d89c2560328861dc71612101674b75
milos.colic <milos.colic@databricks.com>
2022-06-05 16:49:21 +0100

    Add CRSBounds support. Add CRSBounds.csv to resources that supplies bounds captured from spatialreferences.org. Add CRSBoundsProvider that provides APIs to interact with bounds. Add tests.

---
+++ src/test/scala/com/databricks/labs/mosaic/core/crs/TestCRSBoundsProvider.scala
@@ -0,0 +1,37 @@
+package com.databricks.labs.mosaic.core.crs
+
+import com.databricks.labs.mosaic.core.geometry.api.GeometryAPI.{ESRI, JTS}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers._
+
+class TestCRSBoundsProvider extends AnyFlatSpec {
+
+    "CRSBoundsProvider" should "load resource file and return correct bounds for EPSG:4326 and EPSG:27700 for ESRI geometry API" in {
+        val boundsProvider = CRSBoundsProvider(geometryAPI = ESRI)
+        val bounds4326 = boundsProvider.boundary("EPSG", 4326)
+        val bounds27700 = boundsProvider.boundary("EPSG", 27700)
+        bounds4326.lowerLeft.getX shouldBe -180.00
+        bounds4326.lowerLeft.getY shouldBe -90.00
+        bounds4326.upperRight.getX shouldBe 180.00
+        bounds4326.upperRight.getY shouldBe 90.00
+        bounds27700.lowerLeft.getX shouldBe -7.56
+        bounds27700.lowerLeft.getY shouldBe 49.96
+        bounds27700.upperRight.getX shouldBe 1.78
+        bounds27700.upperRight.getY shouldBe 60.84
+    }
+
+    "CRSBoundsProvider" should "load resource file and return correct bounds for EPSG:4326 and EPSG:27700 for JTS geometry API" in {
+        val boundsProvider = CRSBoundsProvider(geometryAPI = JTS)
+        val bounds4326 = boundsProvider.boundary("EPSG", 4326)
+        val bounds27700 = boundsProvider.boundary("EPSG", 27700)
+        bounds4326.lowerLeft.getX shouldBe -180.00
+        bounds4326.lowerLeft.getY shouldBe -90.00
+        bounds4326.upperRight.getX shouldBe 180.00
+        bounds4326.upperRight.getY shouldBe 90.00
+        bounds27700.lowerLeft.getX shouldBe -7.56
+        bounds27700.lowerLeft.getY shouldBe 49.96
+        bounds27700.upperRight.getX shouldBe 1.78
+        bounds27700.upperRight.getY shouldBe 60.84
+    }
+
+}
