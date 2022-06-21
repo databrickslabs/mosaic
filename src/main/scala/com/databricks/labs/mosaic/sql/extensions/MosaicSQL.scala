166f0960c195b327e70000028a9bbafe129e59d0
milos.colic <milos.colic@databricks.com>
2022-06-21 11:14:58 +0100

    Add scala documentation to new SQL registration classes.

--- src/main/scala/com/databricks/labs/mosaic/sql/extensions/MosaicSQL.scala
+++ src/main/scala/com/databricks/labs/mosaic/sql/extensions/MosaicSQL.scala
@@ -7,8 +7,23 @@ import com.databricks.labs.mosaic.functions.MosaicContext
 import org.apache.spark.internal.Logging
 import org.apache.spark.sql.SparkSessionExtensions
 
+/**
+  * Supports automatic registration of SQL expressions at the cluster start up
+  * time. This class registers and injects a rule that based on the configured
+  * index system and configured geometry api adds corresponding SQL expressions
+  * to spark session. These rules are activated just after the spark session has
+  * been created. If unsupported pair of index system and geometry api has been
+  * provided this operation will crash the code in order to make sure correct
+  * functionality has been enabled.
+  */
 class MosaicSQL extends (SparkSessionExtensions => Unit) with Logging {
 
+    /**
+      * Constructor for the MosaicSQL extension. All the registration logic
+      * happens before a nop rule has been injected.
+      * @param ext
+      *   Hook to spark session that is needed for expression registration.
+      */
     override def apply(ext: SparkSessionExtensions): Unit = {
         ext.injectCheckRule(spark => {
             val indexSystem = spark.conf.get("spark.databricks.labs.mosaic.index.system")
@@ -22,6 +37,7 @@ class MosaicSQL extends (SparkSessionExtensions => Unit) with Logging {
             }
             logInfo(s"Registering Mosaic SQL Extensions ($indexSystem, $geometryAPI).")
             mosaicContext.register(spark)
+            // NOP rule. This rule is specified only to respect syntax.
             _ => ()
         })
     }
--- src/main/scala/com/databricks/labs/mosaic/sql/extensions/MosaicSQLDefault.scala
+++ src/main/scala/com/databricks/labs/mosaic/sql/extensions/MosaicSQLDefault.scala
@@ -7,13 +7,27 @@ import com.databricks.labs.mosaic.functions.MosaicContext
 import org.apache.spark.internal.Logging
 import org.apache.spark.sql.SparkSessionExtensions
 
+/**
+  * Supports automatic registration of SQL expressions at the cluster start up
+  * time. This class registers and injects a rule that based on the default
+  * index system and configured geometry api adds corresponding SQL expressions
+  * to spark session. These rules are activated just after the spark session has
+  * been created.
+  */
 class MosaicSQLDefault extends (SparkSessionExtensions => Unit) with Logging {
 
+    /**
+      * Constructor for the MosaicSQL extension. All the registration logic
+      * happens before a NOP rule has been injected.
+      * @param ext
+      *   Hook to spark session that is needed for expression registration.
+      */
     override def apply(ext: SparkSessionExtensions): Unit = {
         ext.injectCheckRule(spark => {
             val mosaicContext = MosaicContext.build(H3IndexSystem, ESRI)
             logInfo(s"Registering Mosaic SQL Extensions (H3, ESRI).")
             mosaicContext.register(spark)
+            // NOP rule. This rule is specified only to respect syntax.
             _ => ()
         })
     }
