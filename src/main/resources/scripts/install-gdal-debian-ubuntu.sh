96c6a44c8ffc923bfa1ab6c6c94c6cf02f01c3be
sllynn <stuart.lynn@databricks.com>
2022-07-19 22:34:56 +0100

    updated gdal install script to work with GH actions

--- src/main/resources/scripts/install-gdal-debian-ubuntu.sh
+++ src/main/resources/scripts/install-gdal-debian-ubuntu.sh
@@ -1,4 +1,4 @@
-apt-get update
-apt-get install -y gdal-bin gdal-data python3-gdal libgdal-dev
+sudo apt-get update
+sudo apt-get install -y gdal-bin gdal-data python3-gdal libgdal-dev
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal
