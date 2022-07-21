46ee573daaa33ee7b51428d8ac0c9cd84523e9ec
robert.whiffin <robert.whiffin@databricks.com>
2022-07-21 09:24:33 +0100

    putting spark back in. If we want to build doc automatically need to have packages loaded

--- R/build_r_package.R
+++ R/build_r_package.R
@@ -23,16 +23,16 @@ for(repo in names(mirror_status)){
   }
 }
 
-#install.packages("devtools", repos=repo)
+install.packages("devtools", repos=repo)
 install.packages("roxygen2", repos=repo)
-#install.packages("sparklyr", repos=repo)
-#devtools::install_github("apache/spark@v3.2.1", subdir='R/pkg')
+install.packages("sparklyr", repos=repo)
+devtools::install_github("apache/spark@v3.2.1", subdir='R/pkg')
 
-#library(devtools)
+library(devtools)
 library(roxygen2)
-#library(SparkR)
-#library(sparklyr)
-#SparkR::install.spark(mirrorUrl="https://archive.apache.org/dist/spark")
+library(SparkR)
+library(sparklyr)
+SparkR::install.spark(mirrorUrl="https://archive.apache.org/dist/spark")
 
 
 
@@ -43,18 +43,14 @@ build_mosaic_bindings <- function(){
   system(system_cmd)
 
   # build doc
-  #devtools::document("sparkR-mosaic/sparkrMosaic")
-  #devtools::document("sparklyr-mosaic/sparklyrMosaic")
-  roxygen2::roxygenize("sparkR-mosaic/sparkrMosaic")
-  roxygen2::roxygenize("sparklyr-mosaic/sparklyrMosaic")
+  devtools::document("sparkR-mosaic/sparkrMosaic")
+  devtools::document("sparklyr-mosaic/sparklyrMosaic")
   
   
   ## build package
-  #devtools::build("sparkR-mosaic/sparkrMosaic")
-  #devtools::build("sparklyr-mosaic/sparklyrMosaic")
-  system("R CMD build sparkR-mosaic/sparkrMosaic")
-  system("R CMD build sparkR-mosaic/sparkrMosaic")
+  devtools::build("sparkR-mosaic/sparkrMosaic")
+  devtools::build("sparklyr-mosaic/sparklyrMosaic")
+  
 }
-
 
 build_mosaic_bindings()
