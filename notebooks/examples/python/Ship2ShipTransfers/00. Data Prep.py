76093e27c1c49a113574b1bbea49d4c203076586
Timo <Timo@Roest.xyz>
2022-07-25 07:36:23 -0400

    Changes to data prep notebook.

--- notebooks/examples/python/Ship2ShipTransfers/00. Data Prep.py
+++ notebooks/examples/python/Ship2ShipTransfers/00. Data Prep.py
@@ -4,6 +4,7 @@
 # COMMAND ----------
 
 import mosaic as mos
+from pyspark.sql.functions import *
 
 spark.conf.set("spark.databricks.labs.mosaic.geometry.api", "ESRI")
 spark.conf.set("spark.databricks.labs.mosaic.index.system", "H3")
@@ -50,7 +51,8 @@ schema = """
 """
 
 AIS_df = (
-    spark.read.option("badRecordsPath", "/tmp/ais_invalid")  # Quarantine bad records
+    # Quarantine bad records
+    spark.read.option("badRecordsPath", "/tmp/ais_invalid")
     .csv("/tmp/vessels/2018", header=True, schema=schema)
     .filter("VesselType = 70")  # Only select cargos
     .filter("Status IS NOT NULL")
@@ -68,94 +70,11 @@ display(AIS_df)
 
 # COMMAND ----------
 
-# MAGIC %md ## Continental US Data
-
-# COMMAND ----------
-
-from pyspark.sql.functions import *
-
-one_metre = 0.00001 - 0.000001
-tolerance = 500 * one_metre
-
-# COMMAND ----------
-
-user_name = (
-    dbutils.notebook.entry_point.getDbutils().notebook().getContext().userName().get()
-)
-
-raw_path = f"dbfs:/tmp/ship2ship/{user_name}"
-raw_us_outline_path = f"{raw_path}/us_outline"
-
-print(f"The raw data is stored in {raw_path}")
-
-# COMMAND ----------
-
-import requests
-import pathlib
-
-us_outline_url = (
-    "https://eric.clst.org/assets/wiki/uploads/Stuff/gz_2010_us_outline_500k.json"
-)
-
-# The DBFS file system is mounted under /dbfs/ directory on Databricks cluster nodes
-
-local_us_outline_path = pathlib.Path(raw_us_outline_path.replace("dbfs:/", "/dbfs/"))
-local_us_outline_path.mkdir(parents=True, exist_ok=True)
-
-req = requests.get(us_outline_url)
-with open(local_us_outline_path / f"us_outline.geojson", "wb") as f:
-    f.write(req.content)
-
-# COMMAND ----------
-
-outlines = (
-    spark.read.format("json")
-    .option("multiline", "true")
-    .load(raw_us_outline_path)
-    .select("type", explode(col("features")).alias("feature"))
-    .select(
-        "type",
-        col("feature.properties").alias("properties"),
-        to_json(col("feature.geometry")).alias("json_geometry"),
-    )
-    .withColumn("geometry", mos.st_aswkt(mos.st_geomfromgeojson("json_geometry")))
-    .withColumn("coastal_area", mos.st_buffer("geometry", lit(tolerance)))
-    .where("properties.type == 'COASTAL'")
-    .drop("json_geometry")
-)
-display(outlines)
-
-# COMMAND ----------
-
-# MAGIC %%mosaic_kepler
-# MAGIC outlines "coastal_area" "geometry" 100
-
-# COMMAND ----------
-
-coastal_h3 = (
-    outlines.withColumn("geometry", mos.st_buffer("geometry", lit(tolerance)))
-    .select(mos.mosaic_explode("geometry", lit(9)).alias("h3"))
-    .select(col("h3.index_id").alias("h3"))
-)
-
-coastal_h3.createOrReplaceTempView("coastal")
-
-display(coastal_h3)
-
-# COMMAND ----------
-
-# MAGIC %%mosaic_kepler
-# MAGIC "coastal" "h3" "h3" 100000
-
-# COMMAND ----------
-
-(coastal_h3.write.mode("overwrite").format("delta").saveAsTable("ship2ship.coastal_h3"))
-
-# COMMAND ----------
-
 # MAGIC %md ## Harbours
 # MAGIC
 # MAGIC This data can be obtained from [here](https://data-usdot.opendata.arcgis.com/datasets/usdot::ports-major/about), and loaded accordingly.
+# MAGIC Choosing `(1*0.001 - 1*0.0001)` as being equal to 99.99 metres at the equator
+# MAGIC Ref: http://wiki.gis.com/wiki/index.php/Decimal_degrees
 
 # COMMAND ----------
 
