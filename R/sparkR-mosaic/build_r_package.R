c8015581dcf619fdedc2a32157068fe5b8b33778
robert.whiffin <robert.whiffin@databricks.com>
2022-08-03 15:58:10 +0100

    updating scala build description, removing old R comments and introducing repo choose logic to R

--- R/sparkR-mosaic/build_r_package.R
+++ R/sparkR-mosaic/build_r_package.R
@@ -1,12 +1,34 @@
-repo<-"https://cran.ma.imperial.ac.uk/"
+repos = c(
+  "https://cran.ma.imperial.ac.uk" = "https://cran.ma.imperial.ac.uk"
+  ,"https://www.stats.bris.ac.uk/R" = "https://www.stats.bris.ac.uk/R"
+  ,"https://cran.rstudio.com/"  = "https://cran.rstudio.com/" 
+)
 
-#install.packages("devtools", repos=repo)
+mirror_is_up <- function(x){
+  out <- tryCatch({
+    available.packages(contrib.url(x))
+  }
+  ,error = function(cond){return(0)}
+  ,warning = function(cond){return(0)}
+  ,finally = function(cond){}
+  )
+  return(length(out))
+}
+
+mirror_status = lapply(repos, mirror_is_up)
+for(repo in names(mirror_status)){
+  if (mirror_status[[repo]] > 1){
+    repo <<- repo
+    break
+  }
+}
+
 install.packages("pkgbuild", repos=repo)
 install.packages("roxygen2", repos=repo)
 
 spark_location <- "/usr/spark-download/unzipped/spark-3.2.1-bin-hadoop2.7"
 Sys.setenv(SPARK_HOME = spark_location)
-#library(devtools)
+
 library(pkgbuild)
 library(roxygen2)
 library(SparkR, lib.loc = c(file.path(spark_location, "R", "lib")))
