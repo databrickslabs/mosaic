3e2345b28c50747e0e03fbb0706bab41e71c94ac
milos.colic <milos.colic@databricks.com>
2022-08-08 12:09:21 +0100

    Add fix missing flag.

--- src/main/resources/scripts/install-gdal-debian-ubuntu.sh
+++ src/main/resources/scripts/install-gdal-debian-ubuntu.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
-sudo apt-get install -y gdal-bin=3.0 gdal-data python3-gdal libgdal-dev libgdal-java
+sudo sudo apt-get install -y --fix-missing gdal-bin=3.0.* gdal-data python3-gdal libgdal-dev libgdal-java
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal
