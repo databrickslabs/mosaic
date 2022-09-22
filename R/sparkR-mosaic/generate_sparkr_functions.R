6efa4f9e446111e669b20b00a3a0b3a8e54d3997
robert.whiffin <robert.whiffin@databricks.com>
2022-09-22 10:41:45 +0100

    updating brace parsing logic

--- R/sparkR-mosaic/generate_sparkr_functions.R
+++ R/sparkR-mosaic/generate_sparkr_functions.R
@@ -103,16 +103,19 @@ get_function_names <- function(scala_file_path){
   start_index = grep(start_string, scala_file, fixed=T) + 1
   # find the methods end - will be the next curly bracket
   # need to find where the matching end brace for the start string is located.
+  # counter starts at 1 as the start string includes the opening brace
   brace_counter = 1
 
   for(i in start_index : length(scala_file)){
-    if(grepl("{", scala_file[i], fixed=T)){
-      brace_counter = brace_counter + 1
-    }
-    else if(grepl("}", scala_file[i], fixed=T)){
-      brace_counter = brace_counter -1
-      if (brace_counter == 0) break
-    }
+    # split the string into characters - returns a list so unlist it
+    line_characters <- unlist(strsplit(scala_file[i], ''))
+    # count the number of brace opens
+    n_opens = sum(grepl("{", line_characters, fixed=T))
+    # count the number of brace closes
+    n_closes = sum(grepl("}", line_characters, fixed=T))
+    # update the counter
+    brace_counter <- brace_counter + n_opens - n_closes
+    if (brace_counter == 0) break
 
   }
   methods_to_bind = scala_file[start_index:i]
