cce6896a9eb0c5a0d20021fff3ba98e29d325cc1
Milos Colic <milos.colic@databricks.com>
2022-10-13 19:12:54 -0400

    Fix scala style errors in github build.

--- src/main/scala/com/databricks/labs/mosaic/core/types/raster/RasterMetadataType.scala
+++ src/main/scala/com/databricks/labs/mosaic/core/types/raster/RasterMetadataType.scala
@@ -3,14 +3,23 @@ package com.databricks.labs.mosaic.core.types.raster
 import com.databricks.labs.mosaic.core.types._
 import org.apache.spark.sql.types._
 
-class RasterMetadataType extends StructType(Array(
-  StructField("xSize", IntegerType),
-  StructField("ySize", IntegerType),
-  StructField("extent", RasterExtentType),
-  StructField("numBands", IntegerType),
-  StructField("crs", StringType),
-  StructField("bands", ArrayType.apply(RasterBandType))
-))
+class RasterMetadataType
+    extends StructType(
+        Array(
+            StructField("xSize", IntegerType),
+            StructField("ySize", IntegerType),
+            StructField("extent", RasterExtentType),
+            StructField("numBands", IntegerType),
+            StructField("crs", StringType),
+            StructField("bands", ArrayType.apply(RasterBandType))
+        )
+    ) {
+
+    override def simpleString: String = "RasterMetadata"
+
+    override def typeName: String = "raster_metadata"
+
+}
 
 
 
