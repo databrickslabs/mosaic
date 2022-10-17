b9b433773b8f6de0da5170d5d703a630cc64bf89
milos-colic <milos-colic>
2022-10-17 01:05:13 +0000

    FInilise the BNG notebook.

--- notebooks/examples/python/BritishNationalGrid.py
+++ notebooks/examples/python/BritishNationalGrid.py
@@ -145,7 +145,7 @@ display(uprns_table)
 
 # COMMAND ----------
 
-uprns_table.write.format("delta").saveAsTable("uprns_bng_table")
+uprns_table.write.format("delta").mode("overwrite").saveAsTable("uprns_bng_table")
 
 # COMMAND ----------
 
@@ -180,14 +180,10 @@ uprns_table.count()
 # COMMAND ----------
 
 from mosaic import MosaicFrame
-from mosaic import MosaicContext
-
-mosaic_context = MosaicContext(spark)
-bng = mosaic_context._context.indexSystem()
 
 postcodes_mosaic_frame = MosaicFrame(postcodes, "geometry")
 optimal_resolution = postcodes_mosaic_frame.get_optimal_resolution(sample_fraction=0.75)
-optimal_resolution_str = bng.getResolutionStr(optimal_resolution)
+optimal_resolution_str = postcodes_mosaic_frame.get_optimal_resolution_str(sample_fraction=0.75)
 
 print(f"""
   Optimal resolution code is :{optimal_resolution}.
@@ -220,6 +216,7 @@ display(
 # MAGIC We will use mosaic sql functions to index our points data. </br>
 # MAGIC Here we will use resolution -4 (500m), index resolution depends on the dataset in use. <br>
 # MAGIC There is a second best choice which is 4 (100m). <br> 
+# MAGIC The user can pass either numerical resolution or the string label to the index expressions. <br>
 # MAGIC BNG provides 2 types of hieracies. <br> 
 # MAGIC The standard hierarchy which operates with index resolutions in base 10 (i.e. (6, 1m), (5, 10m), (4, 100m), (3, 1km), (2, 10km), (1, 100km)) and index ids follow the format of letter pair followed by coordinate bins at the selected resolution (e.g. TQ100100 for (4, 100m)). <br>
 # MAGIC The quad hierachy (or quadrant hierarchy) which operates with index resolutions in base 5 (i.e. (-6, 5m), (-5, 50m), (-4, 500m), (-3, 5km), (-2, 50km), (-1, 500km)) and index ids follow the format of letter pair followed by coordinate bins at the selected resolution and folowed by quadrant letters (e.g. TQ100100SW for (-4, 500m)). Quadrants correspond to compas directions SW (south west), NW (north west), NE (north east) and SE (south east). <br>
@@ -230,7 +227,8 @@ uprns_table = spark.read.table("uprns_bng_table")
 uprns_table = (
   uprns_table
     .withColumn("uprn_bng_500m", mos.grid_pointascellid("uprn_point", lit(optimal_resolution)))
-    .withColumn("uprn_bng_100m", mos.grid_pointascellid("uprn_point", lit(-optimal_resolution)))
+    .withColumn("uprn_bng_500m_str", mos.grid_pointascellid("uprn_point", lit(optimal_resolution_str)))
+    .withColumn("uprn_bng_100m_str", mos.grid_pointascellid("uprn_point", lit("100m")))
 )
 
 # COMMAND ----------
@@ -239,158 +237,11 @@ uprns_table.display()
 
 # COMMAND ----------
 
-import re
-
-import h3
-import pandas as pd
-from IPython.core.magic import Magics, cell_magic, magics_class
-from keplergl import KeplerGl
-from pyspark.sql import DataFrame
-from pyspark.sql.functions import col, conv, lower, lit
-
-from mosaic.api.accessors import st_astext, st_aswkt
-from mosaic.api.constructors import st_geomfromwkt, st_geomfromwkb
-from mosaic.api.functions import st_centroid2D, grid_pointascellid, grid_boundaryaswkb, st_setsrid, st_transform
-from mosaic.config import config
-from mosaic.utils.kepler_config import mosaic_kepler_config
-
-
-external_ref_map = None
-
-@magics_class
-class MosaicKepler2(Magics):
-    @staticmethod
-    def displayKepler(map_instance, height, width):
-        decoded = (
-            map_instance._repr_html_()
-            .decode("utf-8")
-            .replace(".height||400", f".height||{height}")
-            .replace(".width||400", f".width||{width}")
-        )
-        ga_script_redacted = re.sub(
-            r"\<script\>\(function\(i,s,o,g,r,a,m\).*?GoogleAnalyticsObject.*?(\<\/script\>)",
-            "",
-            decoded,
-            flags=re.DOTALL,
-        )
-        async_script_redacted = re.sub(
-            r"s\.a\.createElement\(\"script\",\{async.*?\}\),",
-            "",
-            ga_script_redacted,
-            flags=re.DOTALL,
-        )
-        config.notebook_utils.displayHTML(async_script_redacted)
-
-    @staticmethod
-    def get_spark_df(table_name):
-        try:
-            table = config.ipython_hook.ev(table_name)
-            if isinstance(table, pd.DataFrame):  # pandas dataframe
-                data = config.mosaic_spark.createDataFrame(table)
-            elif isinstance(table, DataFrame):  # spark dataframe
-                data = table
-            else:  # table name
-                data = config.mosaic_spark.read.table(table)
-        except NameError:
-            try:
-                data = config.mosaic_spark.read.table(table_name)
-            except:
-                raise Exception(f"Table name reference invalid.")
-        return data
-
-    @staticmethod
-    def set_centroid(pandas_data, feature_type, feature_name):
-        tmp_sdf = config.mosaic_spark.createDataFrame(pandas_data.iloc[:1])
-        
-        if feature_type == "h3":
-            tmp_sdf = tmp_sdf.withColumn(feature_name, grid_boundaryaswkb(feature_name))
-            
-        centroid = (
-            tmp_sdf.select(st_centroid2D(feature_name))
-            .limit(1)
-            .collect()[0][0]
-        )
-            
-        # set to centroid of a geom
-        mosaic_kepler_config["config"]["mapState"]["latitude"] = centroid[1]
-        mosaic_kepler_config["config"]["mapState"]["longitude"] = centroid[0]
-
-    @cell_magic
-    def mosaic_kepler2(self, *args):
-        """Replace current line with new output"""
-        inputs = [
-            i
-            for i in " ".join(list(args)).replace("\n", " ").replace('"', "").split(" ")
-            if len(i) > 0
-        ]
-
-        if len(inputs) != 3 and len(inputs) != 4:
-            raise Exception(
-                "Mosaic Kepler magic requires table name, feature column and feature type all to be provided. "
-                + "Limit is optional (default 1000)."
-            )
-
-        table_name = inputs[0]
-        feature_name = inputs[1]
-        feature_type = inputs[2]
-        limit_ctn = 1000
-        if len(inputs) == 4:
-            limit_ctn = int(inputs[3])
-        data = self.get_spark_df(table_name)
-        feature_col_dt = [dt for dt in data.dtypes if dt[0] == feature_name][0]
-
-        if feature_type == "h3":
-            if feature_col_dt[1] == "bigint":
-                data = data.withColumn(
-                    feature_name, lower(conv(col(feature_name), 10, 16))
-                )
-        if feature_type == "bng":
-            data = (data
-                .withColumn(feature_name, grid_boundaryaswkb(feature_name))
-                .withColumn(feature_name, st_geomfromwkb(feature_name))
-                .withColumn(
-                    feature_name, 
-                    st_transform(st_setsrid(feature_name, lit(27700)), lit(4326)) 
-                )
-                .withColumn(feature_name, st_aswkt(feature_name)))
-        elif feature_type == "geometry":
-            data = data.withColumn(feature_name, st_astext(col(feature_name)))
-        elif feature_type == "bng_geometry":
-            data = (data
-                .withColumn(feature_name, st_geomfromwkt(st_aswkt(feature_name)))
-                .withColumn(
-                    feature_name, 
-                    st_transform(st_setsrid(feature_name, lit(27700)), lit(4326)) 
-                )
-                .withColumn(feature_name, st_aswkt(feature_name)))
-        else:
-            raise Exception(f"Unsupported geometry type: {feature_type}.")
-
-        allowed_schema = [
-            field.name
-            for field in data.schema.fields
-            if field.dataType.typeName() in ["string", "long", "integer", "double"]
-        ]
-        data = data.select(*allowed_schema)
-        pandas_data = data.limit(limit_ctn).toPandas()
-
-        self.set_centroid(pandas_data, feature_type, feature_name)
-
-        m1 = KeplerGl(config=mosaic_kepler_config)
-        m1.add_data(data=pandas_data, name=table_name)
-        
-        external_ref_map = m1
-
-        self.displayKepler(m1, 800, 1200)
-
-
-# COMMAND ----------
-
-mosaic_kepler_config
-
-# COMMAND ----------
-
-get_ipython().register_magics(MosaicKepler2)
+# MAGIC %md
+# MAGIC Mosaic has a builtin wrappers for KeplerGL map plots using mosaic_kepler IPython magics. <br>
+# MAGIC Mosaic magics automatically handle bng idex system and CRS conversion for you. <br>
+# MAGIC Given that Kepler Plots are rendered on the browser side we are automatically limiting the row count to 1000. <br>
+# MAGIC The end user can override the number of ploted rows by specifying the desired number.
 
 # COMMAND ----------
 
@@ -398,11 +249,7 @@ count_per_index = uprns_table.groupBy("uprn_bng_500m").count().cache()
 
 # COMMAND ----------
 
-count_per_index.display()
-
-# COMMAND ----------
-
-# MAGIC %%mosaic_kepler2
+# MAGIC %%mosaic_kepler
 # MAGIC count_per_index "uprn_bng_500m" "bng" 50
 
 # COMMAND ----------
@@ -425,16 +272,16 @@ postcodes_with_index = (postcodes
 
 # COMMAND ----------
 
-postcodes_with_index.printSchema()
+postcodes_with_index.display()
 
 # COMMAND ----------
 
-to_display = postcodes_with_index.select("properties.*", "mosaic_index.*")
+to_display = postcodes_with_index.select("properties.Name", "mosaic_index.index_id", mos.st_aswkt("mosaic_index.wkb").alias("geometry"))
 
 # COMMAND ----------
 
-# MAGIC %%mosaic_kepler2
-# MAGIC to_display "index_id" "bng" 500
+# MAGIC %%mosaic_kepler
+# MAGIC to_display "geometry" "geometry(27700)" 200
 
 # COMMAND ----------
 
@@ -451,14 +298,15 @@ to_display = postcodes_with_index.select("properties.*", "mosaic_index.*")
 with_postcodes = (
   uprns_table.join(
     postcodes_with_index,
-    uprns_table["uprn_bng_500m"] == postcodes_with_index["mosaic_index.index_id"]
+    uprns_table["uprn_bng_500m"] == postcodes_with_index["mosaic_index.index_id"],
+    how = "right_outer" # to perserve even emtpy chips
   ).where(
     # If the borough is a core chip (the chip is fully contained within the geometry), then we do not need
     # to perform any intersection, because any point matching the same index will certainly be contained in
     # the borough. Otherwise we need to perform an st_contains operation on the chip geometry.
     col("mosaic_index.is_core") | mos.st_contains(col("mosaic_index.wkb"), col("uprn_point"))
   ).select(
-    "properties.*", "uprn_point", "UPRN", "mosaic_index.index_id"
+    "properties.*", "uprn_point", "UPRN", "mosaic_index.index_id", mos.st_aswkt("mosaic_index.wkb").alias("index_geometry")
   )
 )
 
@@ -478,8 +326,15 @@ display(with_postcodes)
 
 # COMMAND ----------
 
-# MAGIC %%mosaic_kepler2
-# MAGIC with_postcodes "index_id" "bng" 5000
+# MAGIC %%mosaic_kepler
+# MAGIC with_postcodes "index_geometry" "geometry(27700)" 5000
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Using mosaic it takes only a few lines of code to produce BNG based heat map and visualise it in Kepler. <br>
+# MAGIC By default the colors wont be affected by the counts and you'd need to change the options in Kepler UI. <br>
+# MAGIC Navigate to the layer, expland it and for the fill color click on the 3 dots icon, then select count as the field for color scaling. 
 
 # COMMAND ----------
 
@@ -487,23 +342,28 @@ properties_per_index = with_postcodes.groupBy("index_id").count()
 
 # COMMAND ----------
 
-# MAGIC %%mosaic_kepler2
+# MAGIC %%mosaic_kepler
 # MAGIC properties_per_index "index_id" "bng" 6000
 
 # COMMAND ----------
 
-postcodes.display()
+# MAGIC %md
+# MAGIC We can do the same 
 
 # COMMAND ----------
 
-properties_per_postcode = with_postcodes.groupBy("Name").count().join(
-  postcodes.select(col("properties.Name"), mos.st_astext("geometry").alias("geometry")), on=["Name"]
-)
+properties_per_chip = with_postcodes.groupBy("index_geometry").count()
 
 # COMMAND ----------
 
-# MAGIC %%mosaic_kepler2
-# MAGIC properties_per_postcode "geometry" "bng_geometry"
+# MAGIC %md
+# MAGIC Note that if you dont use "right_outer" join some chips will be empty. <br>
+# MAGIC This is due to no UPRNs being located in that exact border part between 2 postal areas. <br>
+
+# COMMAND ----------
+
+# MAGIC %%mosaic_kepler
+# MAGIC properties_per_chip "index_geometry" "geometry(27700)" 20000
 
 # COMMAND ----------
 
