311a505dc5d66d5e09a8741dfc9736f392082889
Erni Durdevic <erni.durdevic@databricks.com>
2022-11-04 13:27:27 +0100

    Reverted pom.xml dependencies

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Databricks Inc'
 author = 'Stuart Lynn, Milos Colic, Erni Durdevic, Robert Whiffin, Timo Roest'
 
 # The full version, including alpha/beta/rc tags
-release = "0.3.4"
+release = "v0.3.4"
 
 
 # -- General configuration ---------------------------------------------------
