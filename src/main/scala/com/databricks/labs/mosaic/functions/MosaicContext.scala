a32096dd52a830c152554b1105b61ae5fed729e1
Tim Dikland <timdikland@gmail.com>
2022-11-04 18:07:17 +0100

    implemented st_difference functionality

--- src/main/scala/com/databricks/labs/mosaic/functions/MosaicContext.scala
+++ src/main/scala/com/databricks/labs/mosaic/functions/MosaicContext.scala
@@ -281,6 +281,11 @@ class MosaicContext(indexSystem: IndexSystem, geometryAPI: GeometryAPI) extends 
           (exprs: Seq[Expression]) => ST_ConvexHull(exprs(0), geometryAPI.name)
         )
         registry.registerFunction(
+            FunctionIdentifier("st_difference", database),
+            ST_Difference.registryExpressionInfo(database),
+            (exprs: Seq[Expression]) => ST_Difference(exprs(0), exprs(1), geometryAPI.name)
+        )
+        registry.registerFunction(
           FunctionIdentifier("st_union", database),
           ST_Union.registryExpressionInfo(database),
           (exprs: Seq[Expression]) => ST_Union(exprs(0), exprs(1), geometryAPI.name)
@@ -497,6 +502,7 @@ class MosaicContext(indexSystem: IndexSystem, geometryAPI: GeometryAPI) extends 
         def st_centroid2D(geom: Column): Column = ColumnAdapter(ST_Centroid(geom.expr, geometryAPI.name))
         def st_centroid3D(geom: Column): Column = ColumnAdapter(ST_Centroid(geom.expr, geometryAPI.name, 3))
         def st_convexhull(geom: Column): Column = ColumnAdapter(ST_ConvexHull(geom.expr, geometryAPI.name))
+        def st_difference(geom1: Column, geom2: Column) = ColumnAdapter(ST_Difference(geom1.expr, geom2.expr, geometryAPI.name))
         def st_distance(geom1: Column, geom2: Column): Column = ColumnAdapter(ST_Distance(geom1.expr, geom2.expr, geometryAPI.name))
         def st_dump(geom: Column): Column = ColumnAdapter(FlattenPolygons(geom.expr, geometryAPI.name))
         def st_geometrytype(geom: Column): Column = ColumnAdapter(ST_GeometryType(geom.expr, geometryAPI.name))
