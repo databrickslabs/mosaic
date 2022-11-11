3620d8b6fd860db18521b69b422db416bfa42da5
Erni Durdevic <erni.durdevic@databricks.com>
2022-11-11 22:15:12 +0100

    Refactored test suite

--- src/test/scala/com/databricks/labs/mosaic/core/TestMosaic.scala
+++ src/test/scala/com/databricks/labs/mosaic/core/TestMosaic.scala
@@ -2,12 +2,11 @@ package com.databricks.labs.mosaic.core
 
 import com.databricks.labs.mosaic.core.geometry.api.GeometryAPI.ESRI
 import com.databricks.labs.mosaic.core.index.H3IndexSystem
-import com.databricks.labs.mosaic.expressions.constructors.ConstructorsBehaviors
-import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.funsuite.AnyFunSuite
 
-class TestMosaic extends AnyFlatSpec with ConstructorsBehaviors {
+class TestMosaic extends AnyFunSuite {
 
-  "mosaicFill" should "not return duplicates with H3" in {
+  test("mosaicFill should not return duplicates with H3") {
     // This tests the fix for issue #243 https://github.com/databrickslabs/mosaic/issues/243
     val geom = ESRI.geometry("POLYGON ((4.42 51.78, 4.38 51.78, 4.39 51.83, 4.40 51.83, 4.41 51.8303, 4.417 51.8295, 4.42 51.83, 4.44 51.81, 4.42 51.78))", "WKT")
     val result = Mosaic.mosaicFill(geom, 7, true, H3IndexSystem, ESRI)
