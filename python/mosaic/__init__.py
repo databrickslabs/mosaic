25cc33193256f0b4925e06ecb0e6feae5c63ae01
Erni Durdevic <erni.durdevic@databricks.com>
2023-01-13 18:37:56 +0100

    Update version to 0.3.7-SNAPSHOT

--- python/mosaic/__init__.py
+++ python/mosaic/__init__.py
@@ -3,4 +3,4 @@ from .core import MosaicLibraryHandler, MosaicContext, MosaicFrame
 from .utils.display_handler import displayMosaic
 from .models import SpatialKNN
 
-__version__ = "0.3.6"
+__version__ = "0.3.7-SNAPSHOT"
