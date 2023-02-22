ca66032ea68739b7e4befe8852e45726fadd4d31
Milos Colic <milos.colic@databricks.com>
2023-02-22 14:35:17 -0500

    Fix broken test.

--- src/test/scala/com/databricks/labs/mosaic/DebugShapefile.scala
+++ src/test/scala/com/databricks/labs/mosaic/DebugShapefile.scala
@@ -1,12 +1,14 @@
 package com.databricks.labs.mosaic
 
+import org.apache.spark.sql.test.SharedSparkSessionGDAL
+import org.apache.spark.sql.QueryTest
 import org.gdal.ogr.ogr
 import org.scalatest.funsuite.AnyFunSuite
 
-class DebugShapefile extends AnyFunSuite {
+class DebugShapefile extends QueryTest with SharedSparkSessionGDAL {
 
     test("Debug Shapefile") {
-        val shapefile = "src/test/resources/shapefiles/shapefile.shp"
+        val shapefile = "/binary/shapefile/map.shp"
         val filePath = getClass.getResource(shapefile).getPath
         val dataset = ogr.GetDriverByName("ESRI Shapefile").Open(filePath, 0)
         dataset.GetLayer(0).GetFeature(0).GetFieldAsString("name")
