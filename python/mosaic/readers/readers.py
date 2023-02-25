080174d5bc77445a7c496617febd6a91469cebc3
milos.colic <milos.colic@databricks.com>
2023-02-25 23:14:07 +0000

    Add python bindings for multi read OGR reader.

---
+++ python/mosaic/readers/readers.py
@@ -0,0 +1,9 @@
+from mosaic_data_frame_reader import MosaicDataFrameReader
+from pyspark.sql import SparkSession
+
+def read():
+    """
+    Returns a MosaicDataFrameReader for reading MosaicDataFrames.
+    """
+    spark = SparkSession.builder.getOrCreate()
+    return MosaicDataFrameReader(spark)
