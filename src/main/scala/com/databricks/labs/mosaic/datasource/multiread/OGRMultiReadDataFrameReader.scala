2a304e665a628d89c8965f7ab458f2cd6a95b15e
milos.colic <milos.colic@databricks.com>
2023-02-25 21:58:18 +0000

    Move to generator appraoch for multi read.

--- src/main/scala/com/databricks/labs/mosaic/datasource/multiread/OGRMultiReadDataFrameReader.scala
+++ src/main/scala/com/databricks/labs/mosaic/datasource/multiread/OGRMultiReadDataFrameReader.scala
@@ -1,13 +1,13 @@
 package com.databricks.labs.mosaic.datasource.multiread
 
 import com.databricks.labs.mosaic.datasource.OGRFileFormat
+import com.databricks.labs.mosaic.expressions.util.OGRReadeWithOffset
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.StructType
 
 import scala.collection.convert.ImplicitConversions.`dictionary AsScalaMap`
 import scala.reflect.ClassTag
-import scala.util.Try
 
 class OGRMultiReadDataFrameReader(sparkSession: SparkSession) extends MosaicDataFrameReader(sparkSession) {
 
@@ -15,7 +15,7 @@ class OGRMultiReadDataFrameReader(sparkSession: SparkSession) extends MosaicData
 
     override def load(path: String): DataFrame = load(Seq(path): _*)
 
-    override def load(paths: String*): DataFrame = {
+    def load2(paths: String*): DataFrame = {
         val df = sparkSession.read
             .format("binaryFile")
             .load(paths: _*)
@@ -61,7 +61,7 @@ class OGRMultiReadDataFrameReader(sparkSession: SparkSession) extends MosaicData
 
                     val ds = OGRFileFormat.getDataSource(driverName, path, vsizip)
                     val layer = OGRFileFormat.getLayer(ds, layerNumber, layerName)
-                    //layer.ResetReading()
+                    // layer.ResetReading()
                     val metadata = layer.GetMetadata_Dict().toMap
 
                     val start = readerN * rowCount
@@ -70,22 +70,79 @@ class OGRMultiReadDataFrameReader(sparkSession: SparkSession) extends MosaicData
                     (start.toInt until end.toInt)
                         .map(_ => {
 
-                                val feature = layer.GetNextFeature()
-                                val fields = OGRFileFormat.getFeatureFields(feature, schema.get)
+                            val feature = layer.GetNextFeature()
+                            val fields = OGRFileFormat.getFeatureFields(feature, schema.get)
 
-                                fields.map(_.toString).toSeq
+                            fields.map(_.toString).toSeq
 
-                        }
-                        )
+                        })
 
                 })
             )
 
         df2.toDF("fields_array")
             .select(
-              schema.get.fields.zipWithIndex.map {
-                  case (f, i) => col("fields_array").getItem(i).cast(f.dataType).alias(f.name)
-              }: _*
+              schema.get.fields.zipWithIndex.map { case (f, i) => col("fields_array").getItem(i).cast(f.dataType).alias(f.name) }: _*
+            )
+
+    }
+
+    override def load(paths: String*): DataFrame = {
+        val df = sparkSession.read
+            .format("binaryFile")
+            .load(paths: _*)
+            .select("path")
+
+        OGRFileFormat.enableOGRDrivers()
+        val headPath = df.head().getString(0)
+
+        val driverName = this.extraOptions.get("driverName").getOrElse("")
+        val layerNumber = this.extraOptions.get("layerNumber").getOrElse("0").toInt
+        val layerName = this.extraOptions.get("layerName").getOrElse("")
+        val chunkSize = this.extraOptions.get("chunkSize").getOrElse("5000").toInt
+        val vsizip = this.extraOptions.get("vsizip").getOrElse("false").toBoolean
+        val inferenceLimit = this.extraOptions.get("inferenceLimit").getOrElse("100").toInt
+
+        val ds = OGRFileFormat.getDataSource(driverName, headPath, vsizip)
+        val layer = OGRFileFormat.getLayer(ds, layerNumber, layerName)
+        val partitionCount = 1 + (layer.GetFeatureCount / chunkSize)
+
+        val schema = OGRFileFormat.inferSchemaImpl(driverName, layerNumber, inferenceLimit, vsizip, headPath)
+
+        val ogrReadWithOffset = OGRReadeWithOffset(
+          col("path").expr,
+          col("chunkIndex").expr,
+          Map(
+            "driverName" -> driverName,
+            "layerNumber" -> layerNumber.toString,
+            "layerName" -> layerName,
+            "chunkSize" -> chunkSize.toString,
+            "vsizip" -> vsizip.toString,
+            "inferenceLimit" -> inferenceLimit.toString
+          ),
+          schema.get
+        )
+
+        df
+            .withColumn(
+              "rowCount",
+              lit(chunkSize)
+            )
+            .withColumn(
+              "chunkIndex",
+              explode(array((0 until partitionCount.toInt).map(lit(_)): _*))
+            )
+            .repartition(
+              partitionCount.toInt,
+              col("path"),
+              col("reader_n")
+            )
+            .withColumn(
+              "row_struct",
+              org.apache.spark.sql.adapters.Column(ogrReadWithOffset)
+            )
+            .select(
+              "row_struct.*"
             )
 
     }
---
+++ src/main/scala/com/databricks/labs/mosaic/expressions/util/OGRReadeWithOffset.scala
@@ -0,0 +1,93 @@
+package com.databricks.labs.mosaic.expressions.util
+
+import com.databricks.labs.mosaic.datasource.OGRFileFormat
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.expressions.{BinaryExpression, CollectionGenerator, Expression, ExpressionInfo}
+import org.apache.spark.sql.catalyst.expressions.codegen.CodegenFallback
+import org.apache.spark.sql.types._
+
+import scala.collection.TraversableOnce
+
+case class OGRReadeWithOffset(pathExpr: Expression, chunkIndexExpr: Expression, config: Map[String, String], schema: StructType)
+    extends BinaryExpression
+      with CollectionGenerator
+      with Serializable
+      with CodegenFallback {
+
+    /** Fixed definitions. */
+    override val inline: Boolean = false
+    val driverName: String = config.getOrElse("driverName", "")
+    val layerNumber: Int = config.getOrElse("layerNumber", "0").toInt
+    val layerName: String = config.getOrElse("layerName", "")
+    val chunkSize: Int = config.getOrElse("chunkSize", "5000").toInt
+    val vsizip: Boolean = config.getOrElse("vsizip", "false").toBoolean
+
+    override def collectionType: DataType = schema
+
+    override def position: Boolean = false
+
+    override def elementSchema: StructType = StructType(Seq(StructField("element", schema)))
+
+    override def eval(input: InternalRow): TraversableOnce[InternalRow] = {
+        val path = pathExpr.eval(input).asInstanceOf[String]
+        val chunkIndex = chunkIndexExpr.eval(input).asInstanceOf[Int]
+        OGRFileFormat.enableOGRDrivers()
+
+        val ds = OGRFileFormat.getDataSource(path, driverName, vsizip)
+        val layer = OGRFileFormat.getLayer(ds, layerNumber, layerName)
+
+        val start = chunkIndex * chunkSize
+        val end = math.min(start + chunkSize, layer.GetFeatureCount()).toInt
+        layer.SetNextByIndex(start)
+        for (_ <- start until end) yield {
+            val feature = layer.GetNextFeature()
+            val row = OGRFileFormat.getFeatureFields(feature, schema)
+            InternalRow.fromSeq(Seq(row))
+        }
+    }
+
+    override def left: Expression = pathExpr
+
+    override def right: Expression = chunkIndexExpr
+
+    override def makeCopy(newArgs: Array[AnyRef]): Expression = {
+        val pathExpr = newArgs(0).asInstanceOf[Expression]
+        val chunkIndexExpr = newArgs(1).asInstanceOf[Expression]
+        OGRReadeWithOffset(pathExpr, chunkIndexExpr, config, schema)
+    }
+
+    override protected def withNewChildrenInternal(newLeft: Expression, newRight: Expression): Expression =
+        makeCopy(Array(newLeft, newRight))
+
+}
+
+object OGRReadeWithOffset {
+
+    /** Entry to use in the function registry. */
+    def registryExpressionInfo(db: Option[String]): ExpressionInfo =
+        new ExpressionInfo(
+          classOf[OGRReadeWithOffset].getCanonicalName,
+          db.orNull,
+          "flatten_polygons",
+          """
+            |    _FUNC_(geometry) - The geometry instance can contain both Polygons and MultiPolygons.
+            |    The flattened representation will only contain Polygons.
+            |    MultiPolygon rows will be exploded into Polygon rows
+            """.stripMargin,
+          "",
+          """
+            |    Examples:
+            |      > SELECT _FUNC_(a);
+            |        Polygon ((...))
+            |        Polygon ((...))
+            |        ...
+            |        Polygon ((...))
+            |  """.stripMargin,
+          "",
+          "generator_funcs",
+          "1.0",
+          "",
+          "built-in"
+        )
+
+}
