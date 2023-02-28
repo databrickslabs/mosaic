d167700d9d389006c784d770d17b253649f81069
Erich O'Saben <erich.osaben@databricks.com>
2023-02-27 20:41:03 -0500

    Added applyLimitOffset and supporting functions to DatabricksSQLDialect.java

--- geotools-plugin/src/main/java/com/databricks/geotools/DatabricksSQLDialect.java
+++ geotools-plugin/src/main/java/com/databricks/geotools/DatabricksSQLDialect.java
@@ -34,11 +34,16 @@ public abstract class DatabricksSQLDialect extends PreparedStatementSQLDialect {
 	
 	public DatabricksSQLDialect(JDBCDataStore dataStore, Integer refreshPeriod) {
 		super(dataStore);
+
+		LOGGER.fine("Creating DatabricksSQLDialect");
+
 		this.wktReader = new WKTReader(new GeometryFactory(new PrecisionModel(PrecisionModel.FLOATING), 4326));
     	this.wkbReader = ThreadLocal.withInitial(() -> new WKBReader(new GeometryFactory(new PrecisionModel(PrecisionModel.FLOATING), 4326)));
     	this.metadataTableName = GT_PK_METADATA;
     	this.refreshPeriod = refreshPeriod;
     	this.layersList = new LayersList();
+
+		LOGGER.fine("DatabricksSQLDialect creation complete");
 	}
 	
 	@Override
@@ -48,9 +53,35 @@ public abstract class DatabricksSQLDialect extends PreparedStatementSQLDialect {
 		if (mtn == null) mtn = GT_PK_METADATA;
 		
 		return layersList.includeTable(schemaName, tableName, mtn, dataStore, cx, refreshPeriod);
-        
     }
 	
 	public LayersList getLayersList() {return layersList;}
+
+	@Override
+    public boolean isLimitOffsetSupported() {
+		LOGGER.fine("In DatabricksSQLDialect.isLimitOffsetSupported");
+        
+		return true;
+    }
+
+    @Override
+    public void applyLimitOffset(StringBuffer sql, int limit, int offset) {
+		LOGGER.fine("In DatabricksSQLDialect.applyLimitOffset...");
+		LOGGER.fine("Value of of sql BEFORE limit is applied: " + sql);
+		LOGGER.fine("Value of of limit: " + limit);
+		LOGGER.fine("Value of of offset: " + offset);
+
+        if (limit >= 0 && limit < Integer.MAX_VALUE) {
+            sql.append(" LIMIT " + limit);
+            if (offset > 0) {
+                sql.append(" OFFSET " + offset);
+            }
+        } else if (offset > 0) {
+            sql.append(" LIMIT " + Integer.MAX_VALUE);
+            sql.append(" OFFSET " + offset);
+        }
+		LOGGER.fine("Value of of sql AFTER limit is applied: " + sql);
+
+    }
 	
 }
