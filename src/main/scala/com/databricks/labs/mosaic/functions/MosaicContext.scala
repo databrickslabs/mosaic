8150692a01f00cbab0af0dd666008fff1f1b3bd8
Erni Durdevic <erni.durdevic@databricks.com>
2023-05-09 13:20:57 +0200

    Fixed photon check

--- src/main/scala/com/databricks/labs/mosaic/functions/MosaicContext.scala
+++ src/main/scala/com/databricks/labs/mosaic/functions/MosaicContext.scala
@@ -939,7 +939,12 @@ object MosaicContext extends Logging {
     def checkDBR(spark: SparkSession): Boolean = {
         val sparkVersion = spark.conf.get("spark.databricks.clusterUsageTags.sparkVersion", "")
         val isML = sparkVersion.contains("-ml-")
-        val isPhoton = spark.conf.get("spark.databricks.photon.enabled", "false").toBoolean
+        var isPhoton = false
+        try {
+          // NOTE: spark.conf.get("spark.databricks.photon.enabled", "false) does not work as expected
+          //       It would always overwrite the actual variable value with the default.
+          isPhoton = spark.conf.get("spark.databricks.photon.enabled").toBoolean
+        }
         if (!isML && !isPhoton) {
             // Print out the warnings both to the log and to the console
             logWarning("DEPRECATION WARNING: Mosaic is not supported on the selected Databricks Runtime")
