6d4067ceaa571c7763f8573d2904b473d82c2ae4
Stuart Lynn <stuart.lynn@databricks.com>
2023-10-12 16:14:55 +0100

    restructed core.raster

--- src/main/scala/com/databricks/labs/mosaic/sql/extensions/MosaicSQLDefault.scala
+++ src/main/scala/com/databricks/labs/mosaic/sql/extensions/MosaicSQLDefault.scala
@@ -2,7 +2,7 @@ package com.databricks.labs.mosaic.sql.extensions
 
 import com.databricks.labs.mosaic.core.geometry.api.JTS
 import com.databricks.labs.mosaic.core.index.H3IndexSystem
-import com.databricks.labs.mosaic.core.raster.api.RasterAPI.GDAL
+import com.databricks.labs.mosaic.core.raster.api.GDAL
 import com.databricks.labs.mosaic.functions.MosaicContext
 import org.apache.spark.internal.Logging
 import org.apache.spark.sql.SparkSessionExtensions
