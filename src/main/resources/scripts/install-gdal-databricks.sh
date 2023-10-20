2355885f83306b2a3a6a8f8a1d5db55bd4c267b0
milos.colic <milos.colic@databrikcs.com>
2023-10-20 12:42:38 +0100

    Set env variable in actions for DATABRICKS_ROOT_VIRTUALENV_ENV

--- src/main/resources/scripts/install-gdal-databricks.sh
+++ src/main/resources/scripts/install-gdal-databricks.sh
@@ -17,7 +17,7 @@ pip install databricks-mosaic-gdal==3.4.3
 # -- find the install dir
 # - if this were run in a notebook would use $VIRTUAL_ENV
 # - since it is init script it lands in $DATABRICKS_ROOT_VIRTUALENV_ENV
-GDAL_RESOURCE_DIR=$(find DATABRICKS_ROOT_VIRTUALENV_ENV -name "databricks-mosaic-gdal")
+GDAL_RESOURCE_DIR=$(find $DATABRICKS_ROOT_VIRTUALENV_ENV -name "databricks-mosaic-gdal")
 
 # -- untar files to root
 # - from databricks-mosaic-gdal install dir
