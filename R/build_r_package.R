cb11c4bd605c4aae016901323493b598cc92b367
Stuart Lynn <stuart.lynn@databricks.com>
2023-10-24 11:57:08 +0100

    updated user agent for installing from posit public binaries

--- R/build_r_package.R
+++ R/build_r_package.R
@@ -5,6 +5,9 @@ repos = c(
   ,"https://cran.rstudio.com/"  = "https://cran.rstudio.com/" 
 )
 
+options(HTTPUserAgent = sprintf("R/%s R (%s)", getRversion(), paste(getRversion(), R.version["platform"], R.version["arch"], R.version["os"])))
+options(download.file.extra = sprintf("--header \"User-Agent: R (%s)\"", paste(getRversion(), R.version["platform"], R.version["arch"], R.version["os"])))
+
 mirror_is_up <- function(x){
   out <- tryCatch({
     available.packages(contrib.url(x))
