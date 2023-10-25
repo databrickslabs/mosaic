a6f2fc0f36bf042c5351953a18b0f794f591796b
Stuart Lynn <stuart.lynn@databricks.com>
2023-10-25 13:44:01 +0100

    removed some cruft from the R build files

--- R/sparkR-mosaic/tests.R
+++ R/sparkR-mosaic/tests.R
@@ -1,5 +1,3 @@
-repo<-"https://cran.ma.imperial.ac.uk/"
-
 spark_location <- "/usr/spark-download/unzipped/spark-3.2.1-bin-hadoop2.7"
 Sys.setenv(SPARK_HOME = spark_location)
 library(SparkR, lib.loc = c(file.path(spark_location, "R", "lib")))
