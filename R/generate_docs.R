db84dcb691f2d0f88836159b628c622bd31073c7
Stuart Lynn <stuart.lynn@databricks.com>
2023-10-25 10:13:42 +0100

    split out steps to enable caching of dependencies

--- R/generate_docs.R
+++ R/generate_docs.R
@@ -1,3 +1,7 @@
+spark_location <- "/usr/spark-download/unzipped/spark-3.2.1-bin-hadoop2.7"
+Sys.setenv(SPARK_HOME = spark_location)
+
+library(SparkR, lib.loc = c(file.path(spark_location, "R", "lib")))
 library(roxygen2)
 
 build_mosaic_docs <- function(){
