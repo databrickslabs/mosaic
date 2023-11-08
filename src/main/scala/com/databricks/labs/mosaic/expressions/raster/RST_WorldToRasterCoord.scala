ea13470d9549a73cdf44f174f897d6437ed43ee1
Stuart Lynn <stuart.lynn@databricks.com>
2023-11-08 21:01:23 +0000

    added scala project changes

--- src/main/scala/com/databricks/labs/mosaic/expressions/raster/RST_WorldToRasterCoord.scala
+++ src/main/scala/com/databricks/labs/mosaic/expressions/raster/RST_WorldToRasterCoord.scala
@@ -2,6 +2,7 @@ package com.databricks.labs.mosaic.expressions.raster
 
 import com.databricks.labs.mosaic.core.raster.api.GDAL
 import com.databricks.labs.mosaic.core.raster.gdal.MosaicRasterGDAL
+import com.databricks.labs.mosaic.core.types.model.MosaicRasterTile
 import com.databricks.labs.mosaic.expressions.base.{GenericExpressionFactory, WithExpressionInfo}
 import com.databricks.labs.mosaic.expressions.raster.base.Raster2ArgExpression
 import com.databricks.labs.mosaic.functions.MosaicExpressionConfig
@@ -24,10 +25,10 @@ case class RST_WorldToRasterCoord(
       * Returns the x and y of the raster by applying GeoTransform as a tuple of
       * Integers. This will ensure projection of the raster is respected.
       */
-    override def rasterTransform(raster: => MosaicRasterGDAL, arg1: Any, arg2: Any): Any = {
+    override def rasterTransform(tile: => MosaicRasterTile, arg1: Any, arg2: Any): Any = {
         val xGeo = arg1.asInstanceOf[Double]
         val yGeo = arg2.asInstanceOf[Double]
-        val gt = raster.getRaster.GetGeoTransform()
+        val gt = tile.getRaster.getRaster.GetGeoTransform()
 
         val (x, y) = GDAL.fromWorldCoord(gt, xGeo, yGeo)
         InternalRow.fromSeq(Seq(x, y))
