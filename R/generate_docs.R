5a25e59b402ce6737f4f36c7c3a254ab097c73e8
Stuart Lynn <stuart.lynn@databricks.com>
2023-11-14 15:16:00 +0000

    `testthat` tests for R package

--- R/generate_docs.R
+++ R/generate_docs.R
@@ -1,4 +1,4 @@
-spark_location <- "/usr/spark-download/unzipped/spark-3.2.1-bin-hadoop2.7"
+spark_location <- "/usr/spark-download/unzipped/spark-3.3.2-bin-hadoop3"
 Sys.setenv(SPARK_HOME = spark_location)
 
 library(SparkR, lib.loc = c(file.path(spark_location, "R", "lib")))
