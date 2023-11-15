9c6efe585f1e7d3a8df6ad1b43e0d23e454e89f0
Michael Johns <mjohns@databricks.com>
2023-11-15 17:13:30 -0500

    Update install-gdal-databricks.sh
    script cleanup

--- src/main/resources/scripts/install-gdal-databricks.sh
+++ src/main/resources/scripts/install-gdal-databricks.sh
@@ -4,36 +4,30 @@
 # - Corresponds to DBR 13+
 # - Jammy offers GDAL 3.4.1
 # Author: Michael Johns | mjohns@databricks.com
-# Last Modified: 13 NOV, 2023
+# Last Modified: 15 NOV, 2023
 
 GDAL_VERSION=3.4.1
 
-# -- refresh package info
-#  - ensue updates and backports are available
+# - refresh package info
+# - ensue updates and backports are available
 apt-add-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc)-backports main universe multiverse restricted"
 apt-add-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc)-updates main universe multiverse restricted"
 apt-add-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc)-security main multiverse restricted universe"
 apt-add-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main multiverse restricted universe"
 apt-get update -y
 
-# -- install GDAL
+# - install GDAL
 apt-get install -y gdal-bin libgdal-dev
 
-# -- install GDAL python bindings (from PyPI) for version installed
-#  - see requirements at https://pypi.org/project/pdal/2.3.0/
-#  - make sure GDAL version matches
+# - install GDAL python bindings (from PyPI) for version installed
 apt-get install -y python3-gdal
 
 pip install --upgrade pip
 
-# - being explicit
-# - vs $(gdalinfo --version | grep -Po '(?<=GDAL )[^;]+' | cut -d, -f1)
+# - being explicit with pip
 pip install GDAL==$GDAL_VERSION
 
-# -- add pre-build JNI shared object to the path
-#  - these are coming from Mosaic
-#  - For shared access clusters "/usr/lib/jni" is not a path
-#    in System.getProperty("java.library.path"),
-#    so let's use "/usr/lib"
+# - add pre-build JNI shared object to the path
 wget -P /usr/lib -nc https://github.com/databrickslabs/mosaic/raw/main/src/main/resources/gdal/ubuntu/libgdalalljni.so
 wget -P /usr/lib -nc https://github.com/databrickslabs/mosaic/raw/main/src/main/resources/gdal/ubuntu/libgdalalljni.so.30
+wget -P /usr/lib -nc https://github.com/databrickslabs/mosaic/raw/main/src/main/resources/gdal/ubuntu/libgdalalljni.so.30.0.3
