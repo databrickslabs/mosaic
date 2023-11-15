47ef6d4d2822cb956d0091ad9344ce510b5e251d
Michael Johns <mjohns@databricks.com>
2023-11-15 17:19:42 -0500

    Update install-gdal-volume.sh
    Script cleanup.

--- src/main/resources/scripts/install-gdal-volume.sh
+++ src/main/resources/scripts/install-gdal-volume.sh
@@ -6,38 +6,32 @@
 # - !!! This init script should be run from /Volumes !!!
 # -     It is for Unity Catalog + Shared Clusters
 # Author: Michael Johns | mjohns@databricks.com
-# Last Modified: 14 NOV, 2023
+# Last Modified: 15 NOV, 2023
 
 GDAL_VERSION=3.4.1
 # !!! UPDATE TO YOUR VOLUME DIR !!!
 VOLUME_DIR=/Volumes/geospatial_docs/gdal_artifacts/data
 
-# -- refresh package info
-#  - ensue updates and backports are available
+# - refresh package info
+# - ensue updates and backports are available
 apt-add-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc)-backports main universe multiverse restricted"
 apt-add-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc)-updates main universe multiverse restricted"
 apt-add-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc)-security main multiverse restricted universe"
 apt-add-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main multiverse restricted universe"
 apt-get update -y
 
-# -- install GDAL
+# - install GDAL
 apt-get install -y gdal-bin libgdal-dev
 
-# -- install GDAL python bindings (from PyPI) for version installed
-#  - see requirements at https://pypi.org/project/pdal/2.3.0/
-#  - make sure GDAL version matches
+# - install GDAL python bindings (from PyPI) for version installed
 apt-get install -y python3-gdal
 
 pip install --upgrade pip
 
-# - being explicit
-# - vs $(gdalinfo --version | grep -Po '(?<=GDAL )[^;]+' | cut -d, -f1)
+# - being explicit with pip
 pip install GDAL==$GDAL_VERSION
 
-# -- add pre-build JNI shared object to the path
-#  - For shared access clusters "/usr/lib/jni" is not a path
-#    in System.getProperty("java.library.path"),
-#    so let's use "/usr/lib"
+# - add pre-build JNI shared object to the path
 # !!! These should come from /Volumes !!!
 cp $VOLUME_DIR/libgdalalljni.so /usr/lib
 cp $VOLUME_DIR/libgdalalljni.so.30 /usr/lib
