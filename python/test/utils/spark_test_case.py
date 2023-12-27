76bcf1ac3fbe10c4f16fdb24ec8548f881849236
Michael Johns <mjohns@databricks.com>
2023-12-26 22:29:39 -0500

    removing sc conflict.

--- python/test/utils/spark_test_case.py
+++ python/test/utils/spark_test_case.py
@@ -9,25 +9,31 @@ import unittest
 class SparkTestCase(unittest.TestCase):
     spark = None
     library_location = None
+    log4jref = None
     
     @classmethod
     def setUpClass(cls) -> None:
         cls.library_location = f"{mosaic.__path__[0]}/lib/mosaic-{version('databricks-mosaic')}-jar-with-dependencies.jar"
         if not os.path.exists(cls.library_location):
             cls.library_location = f"{mosaic.__path__[0]}/lib/mosaic-{version('databricks-mosaic')}-SNAPSHOT-jar-with-dependencies.jar"
-        
-        cls.sc=pyspark.SparkContext()
-        cls.sc.setSystemProperty("spark.log.level", "FATAL")
         cls.spark = (
             SparkSession.builder.master("local")
             .config("spark.jars", cls.library_location)
             .getOrCreate()
         )
         cls.spark.conf.set("spark.databricks.labs.mosaic.jar.autoattach", "false")
+        cls.spark.sparkContext.setLogLevel("FATAL")
+        cls.log4jref = cls.spark.sparkContext._jvm.org.apache.log4j
+        cls.log4jref.LogManager.getLogger("org.apache.spark.repl.Main").setLogLevel(cls.log4jref.Level.FATAL)
+        cls.log4jref.LogManager.getRootLogger().setLogLevel(cls.log4jref.Level.FATAL)
+        
+
     
     @classmethod
     def tearDownClass(cls) -> None:
         cls.spark.stop()
 
     def setUp(self) -> None:
-        return super().setUp()
+        self.spark.sparkContext.setLogLevel("FATAL")
+        self.log4jref.LogManager.getLogger("org.apache.spark.repl.Main").setLogLevel(self.log4jref.Level.FATAL)
+        self.log4jref.LogManager.getRootLogger().setLogLevel(self.log4jref.Level.FATAL)
