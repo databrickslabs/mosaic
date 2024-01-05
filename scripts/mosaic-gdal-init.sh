9930f47cd4b76bf55d19940a5b744a4306707741
Michael Johns <mjohns@databricks.com>
2024-01-05 13:57:57 -0500

    numpy rationalization.

--- scripts/mosaic-gdal-init.sh
+++ scripts/mosaic-gdal-init.sh
@@ -26,16 +26,7 @@ WITH_UBUNTUGIS=0 # <- use ubuntugis ppa?
 WITH_FUSE_SO=0   # <- use fuse dir shared objects (vs wget) 
 
 # SPECIFIED VERSIONS 
-# - may be changed by conditional logic
-# - https://docs.scipy.org/doc/scipy/dev/toolchain.html#toolchain-roadmap
-NUMPY_VERSION='==1.22.4' # <- for GDAL + Mosaic
 GDAL_VERSION=3.4.1     # <- ubuntugis is 3.4.3
-
-# - optional: install Mosaic
-if [ $WITH_MOSAIC == 1 ]
-then
-  pip install "databricks-mosaic$MOSAIC_PIP_VERSION"
-fi
 
 # - optional: install GDAL
 if [ $WITH_GDAL == 1 ]
@@ -52,16 +43,8 @@ then
   fi
   sudo apt-get update -y
   
-  # - install specific numpy version
-  # - install scipy version (dep conflict)
-  pip install --upgrade pip
-  pip install "numpy$NUMPY_VERSION"
-  
   # - install natives
-  sudo apt-get install -y gdal-bin libgdal-dev
-
-  # - install gdal
-  pip install gdal==$GDAL_VERSION
+  sudo apt-get install -y gdal-bin libgdal-dev python3-numpy python3-gdal
 
   # - add pre-build JNI shared object to the path
   if [ $WITH_FUSE_SO == 1 ]
@@ -74,9 +57,15 @@ then
     # copy from github
     # TODO: in v0.4.1, include $GITHUB_VERSION
     GITHUB_REPO_PATH=databrickslabs/mosaic/main/resources/gdal/jammy
-    
     sudo wget -nv -P /usr/lib -nc https://raw.githubusercontent.com/$GITHUB_REPO_PATH/libgdalalljni.so
     sudo wget -nv -P /usr/lib -nc https://raw.githubusercontent.com/$GITHUB_REPO_PATH/libgdalalljni.so.30
     sudo wget -nv -P /usr/lib -nc https://raw.githubusercontent.com/$GITHUB_REPO_PATH/libgdalalljni.so.30.0.3
   fi
+fi
+
+# - optional: install Mosaic
+if [ $WITH_MOSAIC == 1 ]
+then
+  pip install --upgrade pip
+  pip install "databricks-mosaic$MOSAIC_PIP_VERSION"
 fi
