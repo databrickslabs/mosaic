a19c82fdca2dfafc4f4788dd921ea03159aa8f12
Milos Colic <milos.chola.cholic@gmail.com>
2025-09-16 09:21:40 +0100

    Add init script. Add so file for gdal311.

---
+++ scripts/mosaic-gdal-init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-add-repository -y "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc)-backports main universe multiverse restricted"
+sudo apt-add-repository -y "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc)-updates main universe multiverse restricted"
+sudo apt-add-repository -y "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc)-security main multiverse restricted universe"
+sudo apt-add-repository -y "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main multiverse restricted universe"
+sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable
+sudo apt-get update -y
+
+# install natives
+sudo apt-get -o DPkg::Lock::Timeout=-1 install -y unixodbc libcurl3-gnutls libsnappy-dev libopenjp2-7
+sudo apt-get -o DPkg::Lock::Timeout=-1 install -y gdal-bin libgdal-dev python3-numpy python3-gdal
+
+# pip install gdal
+pip install --upgrade pip
+pip install gdal==3.11.3
+
+cp /Volumes/[path to the .so file in Volumes]/libgdalalljni.so /usr/lib/libgdalalljni.so
+cp /Volumes/[path to the .jar file in Volumes]/spatial-0.1.0-jar-with-dependencies.jar /databricks/jars
